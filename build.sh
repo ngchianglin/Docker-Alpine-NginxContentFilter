47d2092f01fa20f0665f6ee268ec48bfc00bfdc4
Ng Chiang Lin <ngchianglin@users.noreply.github.com>
2020-08-04 01:00:58 +0800

    Updated build.sh to nginx 1.18.0, pcre 8.44 and openssl 1.1.1g

--- build.sh
+++ build.sh
@@ -3,18 +3,18 @@ apk update
 apk add wget gcc libc-dev make git g++ perl linux-headers gnupg
 mkdir build
 cd build
-wget https://nginx.org/download/nginx-1.16.1.tar.gz
-wget https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz
+wget https://nginx.org/download/nginx-1.18.0.tar.gz
+wget https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.gz
 wget https://www.zlib.net/zlib-1.2.11.tar.gz
-wget https://www.openssl.org/source/openssl-1.1.1d.tar.gz
+wget https://www.openssl.org/source/openssl-1.1.1g.tar.gz
 git clone https://github.com/ngchianglin/NginxContentFilter.git
 
-nginx_sha256="f11c2a6dd1d3515736f0324857957db2de98be862461b5a542a3ac6188dbe32b"
-pcre_sha256="0b8e7465dc5e98c757cc3650a20a7843ee4c3edf50aaf60bb33fd879690d2c73"
+nginx_sha256="4c373e7ab5bf91d34a4f11a0c9496561061ba5eee6020db272a17a7228d35f99"
+pcre_sha256="aecafd4af3bd0f3935721af77b889d9024b2e01d96b58471bd91a3063fb47728"
 zlib_sha256="c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
-openssl_sha256="1e3a91bc1f9dfce01af26026f856e064eab4c8ee0a8f457b5ae30b40b8b711f2"
+openssl_sha256="ddb04774f1e32f0c49751e21b67216ac87852ceb056b75209af2443400636d46"
 content_filter_config="d20e9df127e9e3c87e175b7a2191021a9a3ffc0d94aff5e1dfbdbbaaea033074"
-content_filter_module="299de1516e76be72a643825b9bfcf0feae84ae7294e7b851ec647e528a780199"
+content_filter_module="69dd353620aace2b21c125a24b2562aae53b6ad331836ae17abd966769596d23"
 
 cksum()
 {
@@ -31,17 +31,17 @@ cksum()
   fi
 }
 
-cksum $nginx_sha256 "nginx-1.16.1.tar.gz"
-cksum $pcre_sha256 "pcre-8.43.tar.gz"
+cksum $nginx_sha256 "nginx-1.18.0.tar.gz"
+cksum $pcre_sha256 "pcre-8.44.tar.gz"
 cksum $zlib_sha256 "zlib-1.2.11.tar.gz"
-cksum $openssl_sha256 "openssl-1.1.1d.tar.gz"
+cksum $openssl_sha256 "openssl-1.1.1g.tar.gz"
 cksum $content_filter_config "NginxContentFilter/config"
 cksum $content_filter_module "NginxContentFilter/ngx_http_ct_filter_module.c"
 
-tar -zxvf nginx-1.16.1.tar.gz
-tar -zxvf pcre-8.43.tar.gz
+tar -zxvf nginx-1.18.0.tar.gz
+tar -zxvf pcre-8.44.tar.gz
 tar -zxvf zlib-1.2.11.tar.gz
-tar -zxvf openssl-1.1.1d.tar.gz
+tar -zxvf openssl-1.1.1g.tar.gz
 
 
 #
@@ -50,8 +50,8 @@ tar -zxvf openssl-1.1.1d.tar.gz
 # support _FORTIFY_SOURCE and this option have no 
 # effect 
 #
-cd nginx-1.16.1
-./configure --with-cc-opt="-Wextra -Wformat -Wformat-security -Wformat-y2k -Werror=format-security -fPIE -O2 -D_FORTIFY_SOURCE=2 -fstack-protector-all" --with-ld-opt="-pie -Wl,-z,relro -Wl,-z,now -Wl,--strip-all" --with-http_v2_module --with-http_ssl_module --without-http_uwsgi_module --without-http_fastcgi_module   --without-http_scgi_module --without-http_empty_gif_module --with-openssl=../openssl-1.1.1d --with-openssl-opt="no-ssl2 no-ssl3 no-comp no-weak-ssl-ciphers -O2 -D_FORTIFY_SOURCE=2 -fstack-protector-all -fPIC" --with-zlib=../zlib-1.2.11 --with-zlib-opt="-O2 -D_FORTIFY_SOURCE=2 -fstack-protector-all -fPIC" --with-pcre=../pcre-8.43 --with-pcre-opt="-O2 -D_FORTIFY_SOURCE=2 -fstack-protector-all -fPIC" --with-pcre-jit --add-module=../NginxContentFilter
+cd nginx-1.18.0
+./configure --with-cc-opt="-Wextra -Wformat -Wformat-security -Wformat-y2k -Werror=format-security -fPIE -O2 -D_FORTIFY_SOURCE=2 -fstack-protector-all" --with-ld-opt="-pie -Wl,-z,relro -Wl,-z,now -Wl,--strip-all" --with-http_v2_module --with-http_ssl_module --without-http_uwsgi_module --without-http_fastcgi_module   --without-http_scgi_module --without-http_empty_gif_module --with-openssl=../openssl-1.1.1g --with-openssl-opt="no-ssl2 no-ssl3 no-comp no-weak-ssl-ciphers -O2 -D_FORTIFY_SOURCE=2 -fstack-protector-all -fPIC" --with-zlib=../zlib-1.2.11 --with-zlib-opt="-O2 -D_FORTIFY_SOURCE=2 -fstack-protector-all -fPIC" --with-pcre=../pcre-8.44 --with-pcre-opt="-O2 -D_FORTIFY_SOURCE=2 -fstack-protector-all -fPIC" --with-pcre-jit --add-module=../NginxContentFilter
 make
 make install
 cat << EOF > /usr/local/nginx/conf/nginx.conf
